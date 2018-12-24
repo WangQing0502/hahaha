9f89e545acc4bde6821e45455aad1b9617b300d5
wangqing1992 <3407582841@qq.com>
2018-12-24 17:13:17 +0800

    second

--- redLine/ViewController.m
+++ redLine/ViewController.m
@@ -17,9 +17,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    UIButton * priceDetailButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    priceDetailButton.frame = CGRectMake(240,12.5,60,25);
+    [self.view addSubview:priceDetailButton];
+//    priceDetailButton.titleLabel.font = [AppFont getSystemFont_size:13];
+    [priceDetailButton setTitle:@"价格明细" forState:0];
+    priceDetailButton.contentHorizontalAlignment = NSTextAlignmentRight;
+//    [priceDetailButton setTitleColor:[RGBColor colorWithHexString:lightOrangColor]  forState:0];
+//    [priceDetailButton setTitleColor:[RGBColor colorWithHexString:BlackGrayColor] forState:UIControlStateSelected];
+//    priceDetailButton.layer.cornerRadius = APP_CONTENT_WIDTH_PRAM(8);
+    priceDetailButton.layer.masksToBounds = YES;
+    priceDetailButton.backgroundColor = [UIColor redColor];
+//    [priceDetailButton.layer setBorderColor:[RGBColor colorWithHexString:GrayBlackColor].CGColor];
+//    priceDetailButton.layer.borderWidth =APP_CONTENT_WIDTH_PRAM(0.8);
+    [priceDetailButton addTarget:self action:@selector(priceDetailClick:) forControlEvents:UIControlEventTouchUpInside];
+
+
+
+
 }
 
-
+#pragma mark----价格明细
+- (void)priceDetailClick:(UIButton *)button
+{
+    NSLog(@"dfffff");
+//    PriceDetailViewController * priceDetailCtrl = [[PriceDetailViewController alloc] init];
+//    [self.navigationController pushViewController:priceDetailCtrl animated:NO];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
